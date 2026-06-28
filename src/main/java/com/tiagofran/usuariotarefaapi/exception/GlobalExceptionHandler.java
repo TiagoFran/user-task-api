d6f238f13cbf48a7eed4277edbf4dd681f2b8a69
Tiago Araújo <tiagoeng.sof@gmail.com>
2026-06-28 00:00:00 -0300

    feat: add GlobalExceptionHandler

---
+++ src/main/java/com/tiagofran/usuariotarefaapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.tiagofran.usuariotarefaapi.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
